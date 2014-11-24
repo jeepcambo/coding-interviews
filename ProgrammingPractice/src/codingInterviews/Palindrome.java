45cfb5bcc4d43085266e4528b104c526ebb05cd5
Jeffrey Campbell <campbell.jeffrey.scott@gmail.com>
2014-11-23 20:47:06 -0500

    Next puzzle

---
+++ ProgrammingPractice/src/codingInterviews/Palindrome.java
@@ -0,0 +1,5 @@
+package codingInterviews;
+
+public class Palindrome {
+
+}
--- ProgrammingPractice/src/codingInterviews/StringArrayMatrix.java
+++ ProgrammingPractice/src/codingInterviews/StringArrayMatrix.java
@@ -9,103 +9,104 @@ import java.util.regex.Pattern;
 
 public class StringArrayMatrix {
 
-    private static final class TokenIterator implements Iterator<String> {
+	private static final class TokenIterator implements Iterator<String> {
 
-        private final StringTokenizer tokenizer;
+		private final StringTokenizer tokenizer;
 
-        TokenIterator(String input) {
-            super();
-            this.tokenizer = new StringTokenizer(input);
-        }
+		TokenIterator(String input) {
+			super();
+			this.tokenizer = new StringTokenizer(input);
+		}
 
-        @Override
-        public boolean hasNext() {
-            return tokenizer.hasMoreTokens();
-        }
+		@Override
+		public boolean hasNext() {
+			return tokenizer.hasMoreTokens();
+		}
 
-        @Override
-        public String next() {
-            return tokenizer.nextToken();
-        }
-    }
+		@Override
+		public String next() {
+			return tokenizer.nextToken();
+		}
+	}
 
-    private static final class Tokenizer implements Iterable<String> {
+	private static final class Tokenizer implements Iterable<String> {
 
-        private final String input;
+		private final String input;
 
-        Tokenizer(String input) {
-            super();
-            this.input = input;
-        }
+		Tokenizer(String input) {
+			super();
+			this.input = input;
+		}
 
-        @Override
-        public Iterator<String> iterator() {
-            return new TokenIterator(input);
-        }
-    }
+		@Override
+		public Iterator<String> iterator() {
+			return new TokenIterator(input);
+		}
+	}
 
-    private static final Pattern Number = Pattern.compile("\\d+");
+	private static final Pattern Number = Pattern.compile("\\d+");
 
-    private static final Pattern Operator = Pattern.compile("[-+/*]");
+	private static final Pattern Operator = Pattern.compile("[-+/*]");
 
-    public static int evalRPN(Iterable<String> tokens) {
-        Stack<Integer> stack = new Stack<>();
+	public static int evalRPN(Iterable<String> tokens) {
+		Stack<Integer> stack = new Stack<>();
 
-        try {
-            for (String token : tokens) {
-                if (Operator.matcher(token).matches()) {
-                    int rhs = stack.pop().intValue();
-                    int lhs = stack.pop().intValue();
+		try {
+			for (String token : tokens) {
+				if (Operator.matcher(token).matches()) {
+					int rhs = stack.pop().intValue();
+					int lhs = stack.pop().intValue();
 
-                    switch (token.charAt(0)) {
-                    case '+':
-                        stack.push(Integer.valueOf(lhs + rhs));
-                        break;
-                    case '-':
-                        stack.push(Integer.valueOf(lhs - rhs));
-                        break;
-                    case '*':
-                        stack.push(Integer.valueOf(lhs * rhs));
-                        break;
-                    case '/':
-                        if (rhs == 0) {
-                            throw new ArithmeticException("division by zero");
-                        }
-                        stack.push(Integer.valueOf(lhs / rhs));
-                        break;
-                    }
-                } else if (Number.matcher(token).matches()) {
-                    stack.push(Integer.valueOf(token));
-                } else {
-                    throw new IllegalArgumentException(token);
-                }
-            }
+					switch (token.charAt(0)) {
+					case '+':
+						stack.push(Integer.valueOf(lhs + rhs));
+						break;
+					case '-':
+						stack.push(Integer.valueOf(lhs - rhs));
+						break;
+					case '*':
+						stack.push(Integer.valueOf(lhs * rhs));
+						break;
+					case '/':
+						if (rhs == 0) {
+							throw new ArithmeticException("division by zero");
+						}
+						stack.push(Integer.valueOf(lhs / rhs));
+						break;
+					}
+				} else if (Number.matcher(token).matches()) {
+					stack.push(Integer.valueOf(token));
+				} else {
+					throw new IllegalArgumentException(token);
+				}
+			}
 
-            return stack.pop().intValue();
-        } catch (EmptyStackException e) {
-            // TODO map to better exception
-            return 0;
-        }
-    }
+			return stack.pop().intValue();
+		} catch (EmptyStackException e) {
+			// TODO map to better exception
+			return 0;
+		}
+	}
 
-    public static int evalRPN(String expr) {
-        return evalRPN(new Tokenizer(expr));
-    }
+	public static int evalRPN(String expr) {
+		return evalRPN(new Tokenizer(expr));
+	}
 
-    public static String findLongestPalindrome(@SuppressWarnings("unused") String word) {
-        return null;
-        // TODO return properly
-    }
+	public static String findLongestPalindrome(
+			@SuppressWarnings("unused") String word) {
+		return null;
+		// TODO return properly
+	}
 
-    public static void main(String[] args) {
-        String[] tokens = new String[] { "4", "13", "5", "/", "+" };
+	public static void main(String[] args) {
+		String[] tokens = new String[] { "4", "13", "5", "/", "+" };
 
-        System.out.println(evalRPN(Arrays.asList(tokens)));
+		System.out.println(evalRPN(Arrays.asList(tokens)));
 
-        System.out.println(evalRPN("4 13 5 / +"));
+		System.out.println(evalRPN("4 13 5 / +"));
 
-        for (String word : args) {
-            System.out.println(findLongestPalindrome(word));
-        }
-    }
+		for (String word : args) {
+			System.out.println(findLongestPalindrome(word));
+		}
+	}
 }
